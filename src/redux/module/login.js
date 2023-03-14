521b7ef13886f4fa42418e7abf72a99934b1f151
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-03-15 01:12:36 +0900

    refactor: api 모듈화 작업 중

--- src/redux/module/login.js
+++ src/redux/module/login.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { api } from "../../util/axios";
+import { api } from "../../util/api";
 
 export const __login = createAsyncThunk(
   "loginRadio",
