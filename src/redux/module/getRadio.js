5a254ed022445c8835742b7825e9d1b2f6d642bf
VIVID89 <122258921+VIVID89@users.noreply.github.com>
2023-03-15 16:42:35 +0900

    feat: 전체 라디오 조회 기능 통신 생성 및 테스트 완료

---
+++ src/redux/module/getRadio.js
@@ -0,0 +1,42 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import Axios from '../../util/api/axios';
+
+const axios = new Axios(process.env.REACT_APP_BASE_URL);
+
+export const __getRadio = createAsyncThunk('getRadio', async (radio, thunkAPI) => {
+    return await axios
+        .get(`api/radios/`, radio)
+        .then((response) => {
+            return thunkAPI.fulfillWithValue(response.data);
+        })
+        .catch((error) => console.log(error));
+});
+
+const initialState = {
+    radio: null,
+    isLoading: false,
+    error: null,
+};
+
+const getRadio = createSlice({
+    name: 'getRadio',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(__getRadio.pending, (state) => {
+            state.isLoading = true;
+            state.error = null;
+        });
+        builder.addCase(__getRadio.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.radio = action.payload;
+            state.error = null;
+        });
+        builder.addCase(__getRadio.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        });
+    },
+});
+
+export default getRadio;
