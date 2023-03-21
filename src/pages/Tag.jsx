1b3cd6c8639a17124bd3da5ea309a5daa36a8b60
VIVID89 <122258921+VIVID89@users.noreply.github.com>
2023-03-21 22:15:23 +0900

    format: Tag(카테고리)페이지 코드 수정

--- src/pages/Tag.jsx
+++ src/pages/Tag.jsx
@@ -1,35 +1,35 @@
-import React, { useEffect, useRef } from "react";
-import { useParams } from "react-router-dom";
-import { __getCategoryRadio } from "../redux/module/getRadioCategory";
-import { useDispatch, useSelector } from "react-redux";
-import { useInView } from "react-intersection-observer";
-import RadioContainer from "../components/RadioContainer";
+import React, { useEffect, useRef } from 'react';
+import { useParams } from 'react-router-dom';
+import { __getCategoryRadio } from '../redux/module/getRadioCategory';
+import { useDispatch, useSelector } from 'react-redux';
+import { useInView } from 'react-intersection-observer';
+import RadioContainer from '../components/RadioContainer';
 
 function Tag() {
-  let { id } = useParams();
-  const page = useRef(1);
-  const [ref, inView] = useInView();
-  const dispatch = useDispatch();
-  const data = useSelector((state) => state.gettingRadioCategory);
+    let { id } = useParams();
+    const page = useRef(1);
+    const [ref, inView] = useInView();
+    const dispatch = useDispatch();
+    const data = useSelector((state) => state.gettingRadioCategory);
 
-  useEffect(() => {
-    dispatch(__getCategoryRadio({ categoryType: id, page: page.current }));
-  }, []);
+    useEffect(() => {
+        dispatch(__getCategoryRadio({ categoryType: id, page: page.current }));
+    }, []);
 
-  return (
-    <div>
-      {data?.radio.map((item) => {
-        return item?.data?.map((item, index) => {
-          return (
-            <div key={index}>
-              <span>{item.title}</span>
-              <div></div>
-            </div>
-          );
-        });
-      })}
-    </div>
-  );
+    useEffect(() => {
+        if (inView) {
+            page.current += 1;
+            dispatch(__getCategoryRadio({ page: page.current }));
+        }
+    }, [inView]);
+
+    return (
+        <div>
+            {data?.radio.map((item) => {
+                return <RadioContainer radio={item?.data} />;
+            })}
+        </div>
+    );
 }
 
 export default Tag;
