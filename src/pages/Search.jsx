c8d8387eb1924801072ea60b8badd16b63ee3d4f
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-03-21 11:19:53 +0900

    refactor: Search (useState) 코드 리팩토링

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,117 +1,134 @@
-import React, { useEffect, useState } from 'react';
-import { useForm } from 'react-hook-form';
-import { AiOutlineArrowLeft } from 'react-icons/ai';
-import { useDispatch, useSelector } from 'react-redux';
-import { __searchUser } from '../redux/module/searchUser';
-import styled from 'styled-components';
-import { useNavigate } from 'react-router-dom';
-import RadioContainer from '../components/RadioContainer';
-import SearchHistory from '../components/SearchHistory';
-import RadioUserContainer from '../components/RadioUserContainer';
-import { addSearchHistory } from '../util/getUpdatedHistory';
-import SearchTap from '../components/SearchTap';
+import React, { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import { useDispatch, useSelector } from "react-redux";
+import { __searchUser } from "../redux/module/searchUser";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import RadioContainer from "../components/RadioContainer";
+import SearchHistory from "../components/SearchHistory";
+import RadioUserContainer from "../components/RadioUserContainer";
+import { addSearchHistory } from "../util/getUpdatedHistory";
+import SearchTap from "../components/SearchTap";
 
 function Search() {
-    const [isSearch, setIsSearch] = useState(false);
-    const [sort, setSort] = useState(true);
-    const [searchInfo, setSearchInfo] = useState();
-    const [searchHistory, setSearchHistory] = useState([]);
-    const navigate = useNavigate();
-    const {
-        searchingUser: { user },
-        searchingRadio: { live },
-    } = useSelector((state) => state);
-    const { register, handleSubmit } = useForm();
+  const [isSearch, setIsSearch] = useState(false);
+  const [state, setState] = useState({
+    sort: true,
+    searchInfo: "",
+    searchHistory: [],
+  });
 
-    const dispatch = useDispatch();
+  const { sort, searchHistory, searchInfo } = state;
 
-    useEffect(() => {
-        const histories = localStorage.getItem('searchHistory');
-        if (histories) {
-            setSearchHistory(JSON.parse(histories));
-        }
-    }, []);
+  const navigate = useNavigate();
+  const {
+    searchingUser: { user },
+    searchingRadio: { live },
+  } = useSelector((state) => state);
 
-    const onSearch = (data) => {
-        setSearchInfo(data.searchValue);
-        setIsSearch(true);
-        dispatch(__searchUser(data.searchValue));
-        addSearchHistory({ newKeyword: data.searchValue, searchHistory: [...searchHistory] });
-    };
+  const { register, handleSubmit } = useForm();
+  const dispatch = useDispatch();
 
-    return (
+  useEffect(() => {
+    const histories = localStorage.getItem("searchHistory");
+    if (histories) {
+      setState({
+        ...state,
+        searchHistory: JSON.parse(histories),
+      });
+    }
+  }, []);
+
+  const onSearch = (data) => {
+    setState({
+      ...state,
+      searchInfo: data.searchValue,
+    });
+    setIsSearch(true);
+    dispatch(__searchUser(data.searchValue));
+    addSearchHistory({
+      newKeyword: data.searchValue,
+      searchHistory: [...searchHistory],
+    });
+  };
+
+  return (
+    <>
+      <SearchbarBox>
+        <AiOutlineArrowLeft
+          cursor={"pointer"}
+          size={20}
+          onClick={() => {
+            document.startViewTransition(() => navigate("/"));
+          }}
+        />
+        <form onSubmit={handleSubmit(onSearch)}>
+          <SearchInput
+            {...register("searchValue", { required: "검색어를 입력해주세요." })}
+            type="text"
+            placeholder="검색어를 입력해주세요."
+          />
+        </form>
+      </SearchbarBox>
+      {isSearch ? (
         <>
-            <SearchbarBox>
-                <AiOutlineArrowLeft
-                    cursor={'pointer'}
-                    size={20}
-                    onClick={() => {
-                        document.startViewTransition(() => navigate('/'));
-                    }}
-                />
-                <form onSubmit={handleSubmit(onSearch)}>
-                    <SearchInput
-                        {...register('searchValue', { required: '검색어를 입력해주세요.' })}
-                        type="text"
-                        placeholder="검색어를 입력해주세요."
-                    />
-                </form>
-            </SearchbarBox>
-            {isSearch ? (
-                <>
-                    <SearchTap
-                        searchInfo={searchInfo}
-                        sort={sort}
-                        setIsSearch={setIsSearch}
-                        setSort={setSort}
-                    />
-                    {sort ? (
-                        <>
-                            {user?.data.length === 0 ? (
-                                <div>검색 결과가 없습니다. </div>
-                            ) : (
-                                <RadioUserContainer user={user?.data} />
-                            )}
-                        </>
-                    ) : (
-                        <>
-                            {' '}
-                            {live?.data.length === 0 ? (
-                                <div>검색 결과가 없습니다.</div>
-                            ) : (
-                                <RadioContainer radio={live?.data} />
-                            )}
-                        </>
-                    )}
-                </>
-            ) : (
-                <SearchHistory setIsSearch={setIsSearch} setSearchInfo={setSearchInfo} />
-            )}
+          <SearchTap
+            searchInfo={searchInfo}
+            sort={sort}
+            setIsSearch={setIsSearch}
+            setSort={(value) => setState({ ...state, sort: value })}
+          />
+          {sort ? (
+            <>
+              {user?.data.length === 0 ? (
+                <div>검색 결과가 없습니다. </div>
+              ) : (
+                <RadioUserContainer user={user?.data} />
+              )}
+            </>
+          ) : (
+            <>
+              {" "}
+              {live?.data.length === 0 ? (
+                <div>검색 결과가 없습니다.</div>
+              ) : (
+                <RadioContainer radio={live?.data} />
+              )}
+            </>
+          )}
         </>
-    );
+      ) : (
+        <SearchHistory
+          setIsSearch={setIsSearch}
+          setSearchInfo={(value) => setState({ ...state, searchInfo: value })}
+        />
+      )}
+    </>
+  );
 }
 
 export default Search;
 
 const SearchbarBox = styled.div`
-    width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: flex-start;
-    padding: 0px 30px;
-    min-height: 3.125rem;
-    margin-top: 2.5rem;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  padding: 0px 30px;
+  min-height: 3.125rem;
+  margin-top: 2.5rem;
 `;
 
 const SearchInput = styled.input`
-    width: 400px;
-    height: 35px;
-    padding-left: 50px;
-    border-radius: 15px;
-    margin-left: 20px;
-    font-size: 16px;
-    background: url('https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-256.png')
-        13px center / contain no-repeat #9b9a9ace;
-    border: none;
-    outline: none;
+  width: 400px;
+  height: 35px;
+  padding-left: 50px;
+  border-radius: 15px;
+  margin-left: 20px;
+  font-size: 16px;
+  background: url("https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-256.png")
+    13px center / contain no-repeat #9b9a9ace;
+  border: none;
+  outline: none;
 `;
--- src/util/getUpdatedHistory.js
+++ src/util/getUpdatedHistory.js
@@ -1,16 +1,6 @@
-export const getUpdatedHistory = (newKeyword, searchHistory) => {
-    let updatedHistory = [...searchHistory];
-    updatedHistory.unshift(newKeyword);
-    updatedHistory = [...new Set(updatedHistory)].slice(0, 10);
-    return updatedHistory;
-};
-
-export const setSearchHistory = (updatedHistory) => {
-    localStorage.setItem('searchHistory', JSON.stringify(updatedHistory));
-};
-
-export const addSearchHistory = ({ newKeyword, searchHistory: [...searchHistory] }) => {
-    console.log(newKeyword, searchHistory);
-    const updatedHistory = getUpdatedHistory(newKeyword, searchHistory);
-    setSearchHistory(updatedHistory);
+export const addSearchHistory = ({ newKeyword, searchHistory }) => {
+  let updatedHistory = [...searchHistory];
+  updatedHistory.unshift(newKeyword);
+  updatedHistory = [...new Set(updatedHistory)].slice(0, 10);
+  localStorage.setItem("searchHistory", JSON.stringify(updatedHistory));
 };
