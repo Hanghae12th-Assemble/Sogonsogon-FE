b9135548bbece60879b660047866f89a87ace780
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-03-24 22:20:55 +0900

    format: getRadio 무한 스크롤 적용중

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,73 +1,89 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { __getRadio } from '../redux/module/getRadio';
-import Navbar from '../components/Navbar';
-import { AiOutlinePlus, AiOutlineArrowUp, AiOutlineMenu, AiOutlineSearch } from 'react-icons/ai';
-import Lnb from '../components/Lnb';
-import RadioContainer from '../components/RadioContainer';
-import { useNavigate } from 'react-router-dom';
-import styled from 'styled-components';
-import Button from '../elements/Button';
-import useScroll from '../hooks/useScroll';
+import React, { useEffect, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { __getRadio } from "../redux/module/getRadio";
+import Navbar from "../components/Navbar";
+import {
+  AiOutlinePlus,
+  AiOutlineArrowUp,
+  AiOutlineMenu,
+  AiOutlineSearch,
+} from "react-icons/ai";
+import Lnb from "../components/Lnb";
+import RadioContainer from "../components/RadioContainer";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import Button from "../elements/Button";
+import useScroll from "../hooks/useScroll";
+import { useInView } from "react-intersection-observer";
+import { initInfinitiScroll } from "../redux/module/getRadioCategory";
 
 function Home() {
-    const [isLnbOpen, setIsLnbOpen] = useState(false);
-    const radioContainerRef = useRef();
-    const scrollPos = useScroll(radioContainerRef);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
+  const [isLnbOpen, setIsLnbOpen] = useState(false);
+  const data = useSelector((state) => state.gettingRadio);
+  console.log(data);
+  const page = useRef(1);
+  const [ref, inView] = useInView();
+  const radioContainerRef = useRef();
+  const scrollPos = useScroll(radioContainerRef);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        dispatch(__getRadio());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(initInfinitiScroll());
+    dispatch(__getRadio(page.current));
+  }, []);
 
-    const { radio } = useSelector((state) => {
-        return state.gettingRadio;
-    });
+  useEffect(() => {
+    if (inView) {
+      page.current += 1;
+      dispatch(__getRadio(page.current));
+    }
+  }, [inView]);
 
-    const toggleLnb = () => setIsLnbOpen((prev) => !prev);
+  const toggleLnb = () => setIsLnbOpen((prev) => !prev);
 
-    return (
-        <>
-            <Lnb isOpen={isLnbOpen} handleItemClick={toggleLnb} />
-            <NavbarContainer>
-                <Navbar
-                    iconleft={<AiOutlineMenu size={20} onClick={toggleLnb} />}
-                    title={'소곤소곤'}
-                    iconright={<AiOutlineSearch size={20} />}
-                    toClose={'/search'}
-                />
-            </NavbarContainer>
-            <StRadioContainer ref={radioContainerRef}>
-                <RadioContainer radio={radio?.data} />
-            </StRadioContainer>
-            {scrollPos > 500 && (
-                <Button
-                    TopBtn
-                    onClick={() =>
-                        radioContainerRef.current.scrollTo({ top: 0, behavior: 'smooth' })
-                    }
-                >
-                    <AiOutlineArrowUp size={20} />
-                </Button>
-            )}
+  return (
+    <>
+      <Lnb isOpen={isLnbOpen} handleItemClick={toggleLnb} />
+      <NavbarContainer>
+        <Navbar
+          iconleft={<AiOutlineMenu size={20} onClick={toggleLnb} />}
+          title={"소곤소곤"}
+          iconright={<AiOutlineSearch size={20} />}
+          toClose={"/search"}
+        />
+      </NavbarContainer>
+      <StRadioContainer ref={radioContainerRef}>
+        {data?.radio?.result}
+        <div ref={ref}></div>
+      </StRadioContainer>
+      {scrollPos > 500 && (
+        <Button
+          TopBtn
+          onClick={() =>
+            radioContainerRef.current.scrollTo({ top: 0, behavior: "smooth" })
+          }
+        >
+          <AiOutlineArrowUp size={20} />
+        </Button>
+      )}
 
-            <Button
-                AddRadioBtn
-                onClick={() => {
-                    document.startViewTransition(() => navigate('/createradio'));
-                }}
-            >
-                <AiOutlinePlus size={20} />
-            </Button>
-        </>
-    );
+      <Button
+        AddRadioBtn
+        onClick={() => {
+          document.startViewTransition(() => navigate("/createradio"));
+        }}
+      >
+        <AiOutlinePlus size={20} />
+      </Button>
+    </>
+  );
 }
 
 export default Home;
 
 export const NavbarContainer = styled.div`
-    padding: 0px 20px;
+  padding: 0px 20px;
 `;
 
 // const AddRadioBtn = styled.div`
@@ -87,13 +103,13 @@ export const NavbarContainer = styled.div`
 // `;
 
 export const StRadioContainer = styled.div`
-    /* border: 1px solid black; */
-    position: relative;
-    width: 100%;
-    display: grid;
-    grid-template-columns: repeat(2, 2fr);
-    flex-direction: row;
-    padding: 0px 20px;
-    z-index: -1;
-    overflow: auto;
+  /* border: 1px solid black; */
+  position: relative;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 2fr);
+  flex-direction: row;
+  padding: 0px 20px;
+  z-index: -1;
+  overflow: auto;
 `;
