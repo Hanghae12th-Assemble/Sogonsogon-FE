bf28766b92d84d87bad062bce844852038d16a57
Tbug-debug <76459231+Tbug-debug@users.noreply.github.com>
2023-04-03 14:40:44 +0900

    format: pages 파일 이름 변경에 따른 코드 수정

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Creatradio from "../pages/Createradio";
+import CreateAudio from "../pages/CreateAudio";
 import Profile from "../pages/Profile";
 import Tag from "../pages/Tag";
 import Selectlogin from "../pages/SelectLogin";
@@ -22,7 +22,7 @@ function Router() {
       <Routes>
         <Route path="/*" element={<Notfound />} />
         <Route path="/" element={<Home />} />
-        <Route path={`/${routers.CREATERADIO}`} element={<Creatradio />} />
+        <Route path={`/${routers.CREATERADIO}`} element={<CreateAudio />} />
         <Route path={`/${routers.SELECTLOGIN}`} element={<Selectlogin />} />
         <Route path={`/${routers.SIGNUP}`} element={<Signup />} />
         <Route path={`/${routers.LISTENRADIO}`} element={<ListenRadio />} />
