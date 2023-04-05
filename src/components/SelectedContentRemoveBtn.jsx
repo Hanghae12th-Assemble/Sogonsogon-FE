1385656a098d788968ad4463a8a5e92d40c45eb5
VIVID89 <122258921+VIVID89@users.noreply.github.com>
2023-04-05 19:35:45 +0900

    format: 컨텐트 선택 및 삭제 버튼 컴포넌트화

---
+++ src/components/SelectedContentRemoveBtn.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+
+function SelectedContentRemoveBtn({ state, setState, selectedContent, __removeContent }) {
+    const dispatch = useDispatch();
+    const removeSelectedContent = () => {
+        selectedContent.forEach((contentId) => {
+            dispatch(__removeContent(contentId));
+        });
+        setState({ ...state, selectedContent: [] });
+    };
+
+    return (
+        <>
+            {selectedContent.length > 0 && (
+                <SelectedContentContainer>
+                    <SelectedBtn onClick={removeSelectedContent}>
+                        삭제 {selectedContent?.length}
+                    </SelectedBtn>
+                </SelectedContentContainer>
+            )}
+        </>
+    )
+}
+
+export default SelectedContentRemoveBtn
+
+const SelectedContentContainer = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    margin-top: 10px;
+`;
+
+
+const SelectedBtn = styled.button`
+    width: 100%;
+    height: 80px;
+    font-size: 18px;
+    border: none;
+    background-color: #ff9900;
+    color: white;
+    cursor: pointer;
+`;
