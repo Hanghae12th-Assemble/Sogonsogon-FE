1217eb279727b67d466e0966a7788dfadf51ca28
VIVID89 <122258921+VIVID89@users.noreply.github.com>
2023-04-08 10:20:20 +0900

    feat: 카테고리페이지 get 통신 및 무한스크롤 구현

--- src/redux/module/getAlbumCategory.js
+++ src/redux/module/getAlbumCategory.js
@@ -1,45 +1,50 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import Axios from "../../util/api/axios";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import Axios from '../../util/api/axios';
 
 const axios = new Axios(process.env.REACT_APP_BASE_URL);
 
 export const __getAlbumCategory = createAsyncThunk(
-  "getAlbumCategory",
-  async ({ page, categoryType }, thunkAPI) => {
-    return await axios
-      .get(
-        `api/audioAlbum/${categoryType}?page=${page}&size=5&sortBy=createdAt`
-      )
-      .then((response) => alert(response && "앨범 조회에 성공하였습니다."))
-      .catch((error) => alert(error && "앨범 조회에 실패하였습니다."));
-  }
+    'getAlbumCategory',
+    async ({ page, categoryType }, thunkAPI) => {
+        return await axios
+            .get(`api/audioAlbum/${categoryType}?page=${page}&size=5&sortBy=createdAt`)
+            .then((response) => thunkAPI.fulfillWithValue(response.data))
+            .catch((error) => alert(error && '앨범 조회에 실패하였습니다.'));
+    }
 );
 
 const initialState = {
-  album: null,
-  isLoading: false,
-  error: null,
+    album: [],
+    isLoading: false,
+    error: null,
 };
 
 const getAlbumCategory = createSlice({
-  name: "getAlbumCategory",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(__getAlbumCategory.pending, (state) => {
-      state.isLoading = true;
-      state.error = null;
-    });
-    builder.addCase(__getAlbumCategory.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.album = action.payload;
-      state.error = null;
-    });
-    builder.addCase(__getAlbumCategory.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    });
-  },
+    name: 'getAlbumCategory',
+    initialState,
+    reducers: {
+        initInfinitiScroll: (state, action) => {
+            state.isLoading = false;
+            state.error = false;
+            state.album = [];
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(__getAlbumCategory.pending, (state) => {
+            state.isLoading = true;
+            state.error = null;
+        });
+        builder.addCase(__getAlbumCategory.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.album = [...state.album].concat(action.payload);
+            state.error = null;
+        });
+        builder.addCase(__getAlbumCategory.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        });
+    },
 });
 
+export const { initInfinitiScroll } = getAlbumCategory.actions;
 export default getAlbumCategory;
