682e415756c063687f203dbf1dc3155acb8d00f1
VIVID89 <122258921+VIVID89@users.noreply.github.com>
2023-04-08 10:21:48 +0900

    feat: 클립 모두보기 페이지 get 통신 및 데이터 출력 완료

---
+++ src/redux/module/geClips.js
@@ -0,0 +1,47 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import Axios from '../../util/api/axios';
+
+const axios = new Axios(process.env.REACT_APP_BASE_URL);
+
+export const __getClips = createAsyncThunk('getClips', async ({ id, page }, thunkAPI) => {
+    return await axios
+        .get(`api/audioclip/clips/${id}?page=${page}&size=10&sortBy=createdAt`)
+        .then((response) => thunkAPI.fulfillWithValue(response.data))
+        .catch((error) => alert(error && '앨범 조회에 실패하였습니다.'));
+});
+
+const initialState = {
+    clip: [],
+    isLoading: false,
+    error: null,
+};
+
+const getClips = createSlice({
+    name: 'getClips',
+    initialState,
+    reducers: {
+        initInfinitiScroll: (state, action) => {
+            state.isLoading = false;
+            state.error = false;
+            state.clip = [];
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addCase(__getClips.pending, (state) => {
+            state.isLoading = true;
+            state.error = null;
+        });
+        builder.addCase(__getClips.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.clip = [...state.clip].concat(action.payload);
+            state.error = null;
+        });
+        builder.addCase(__getClips.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        });
+    },
+});
+
+export const { initInfinitiScroll } = getClips.actions;
+export default getClips;
