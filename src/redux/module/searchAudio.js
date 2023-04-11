91a2a5612c1fb2599aa028ad8d9ee72ff1596272
VIVID89 <122258921+VIVID89@users.noreply.github.com>
2023-04-11 16:17:43 +0900

    design: 검색 페이지 탭 디자인 수정

--- src/redux/module/searchAudio.js
+++ src/redux/module/searchAudio.js
@@ -1,45 +1,42 @@
-import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import Axios from "../../util/api/axios";
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import Axios from '../../util/api/axios';
 
 const axios = new Axios(process.env.REACT_APP_BASE_URL);
 
-export const __searchAudio = createAsyncThunk(
-  "searchAudio",
-  async (searchInfo, thunkAPI) => {
+export const __searchAudio = createAsyncThunk('searchAudio', async (searchInfo, thunkAPI) => {
     return await axios
-      .get(`api/radios/find?title=${searchInfo}`)
-      .then((response) => {
-        return thunkAPI.fulfillWithValue(response?.data);
-      })
-      .catch((error) => error);
-  }
-);
+        .get(`api/radios/find?title=${searchInfo}`)
+        .then((response) => {
+            return thunkAPI.fulfillWithValue(response?.data);
+        })
+        .catch((error) => error.message);
+});
 
 const initialState = {
-  live: null,
-  isLoading: false,
-  error: null,
+    live: null,
+    isLoading: false,
+    error: null,
 };
 
 const searchAudio = createSlice({
-  name: "searchAudio",
-  initialState,
-  reducers: {},
-  extraReducers: (builder) => {
-    builder.addCase(__searchAudio.pending, (state) => {
-      state.isLoading = true;
-      state.error = null;
-    });
-    builder.addCase(__searchAudio.fulfilled, (state, action) => {
-      state.isLoading = false;
-      state.live = action.payload;
-      state.error = null;
-    });
-    builder.addCase(__searchAudio.rejected, (state, action) => {
-      state.isLoading = false;
-      state.error = action.payload;
-    });
-  },
+    name: 'searchAudio',
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder.addCase(__searchAudio.pending, (state) => {
+            state.isLoading = true;
+            state.error = null;
+        });
+        builder.addCase(__searchAudio.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.live = action.payload;
+            state.error = null;
+        });
+        builder.addCase(__searchAudio.rejected, (state, action) => {
+            state.isLoading = false;
+            state.error = action.payload;
+        });
+    },
 });
 
 export default searchAudio;
